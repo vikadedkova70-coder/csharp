14891208de6cdeb08a2db5dd2a1acf2ec8fb9674
Vika <vikadedkova70@gmail.com>
2026-06-15 00:12:28 +0500

    Third commit
    WASD (в консоли)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,28 @@ public class Player : MonoBehaviour
 {
     private void Update()
     {
+        Vector2 inputVector = new Vector2(0, 0);
+
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Pressed");
+            inputVector.y = 1f;
         }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1f;
+        }
+
+        Debug.Log(inputVector);
     }
 }
