71908bdc7c9582036a89cabafb3a8e438645874a
Vika <vikadedkova70@gmail.com>
2026-06-14 23:03:46 +0500

    First Commit
    Тест (разбираюсь с Unity)

---
+++ Assets/Scripts/NewEmptyCSharpScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NewEmptyCSharpScript : MonoBehaviour
+{
+    private void Update()
+    {
+        PrintConsole();
+    }
+    private void PrintConsole()
+    {
+        Debug.Log("Hello world");
+    }
+}
