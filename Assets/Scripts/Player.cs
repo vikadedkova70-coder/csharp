495ed5274403561e44ba9e2422d73f4529a4a4cf
Vika <vikadedkova70@gmail.com>
2026-06-15 14:13:03 +0500

    Eigth Commit
    Готовые движения (надеюсь)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float movingSpeed = 5f;
 
-    private PlayerInputActions playerInputActions;
-
     private Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerInputActions = new PlayerInputActions();
-        playerInputActions.Enable();
     }
 
-    private Vector2 GetMovementVector()
-    {
-        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
-
-        return inputVector;
-    }
     private void FixedUpdate()
     {
-        Vector2 inputVector = GetMovementVector();
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
 
         inputVector = inputVector.normalized;
 
