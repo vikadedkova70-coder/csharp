e80ecf7ed53faa4aa9f2ac95d65cd5ff0bcff5f4
Vika <vikadedkova70@gmail.com>
2026-06-21 18:45:00 +0500

    Nineteenth Commit
    Дорабатываю скелет 1

--- Assets/Scripts/Enemies/SkeletonVisual.cs
+++ Assets/Scripts/Enemies/SkeletonVisual.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class SkeletonVisual : MonoBehaviour
 {
     [SerializeField] private EnemyAI _enemyAI;
     [SerializeField] private EnemyEntity _enemyEntity;
+    [SerializeField] private GameObject _shadow;
 
     private Animator _animator;
 
     private const string IS_RUNNING = "IsRunning";
     private const string CHASING_SPEED_MULTIPLIER = "ChasingSpeedMultiplier";
     private const string ATTACK = "Attack";
+    private const string TAKE_HIT = "TakeHit";
+    private const string IS_DEATH = "IsDeath";
+
+    SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         _enemyAI.OnEnemyAttack += _enemyAI_OnEnemyAttack;
+        _enemyEntity.OnTakeHit += _enemyEntity_OnTakeHit;
+        _enemyEntity.OnDeath += _enemyEntity_OnDeath;
     }
 
     private void OnDestroy()
@@ -46,5 +55,17 @@ public class SkeletonVisual : MonoBehaviour
     private void _enemyAI_OnEnemyAttack(object sender, System.EventArgs e)
     {
         _animator.SetTrigger(ATTACK);
+    }
+
+    private void _enemyEntity_OnTakeHit(object sender, System.EventArgs e)
+    {
+        _animator.SetTrigger(TAKE_HIT);
+    }
+
+    private void _enemyEntity_OnDeath(object sender, System.EventArgs e)
+    {
+        _animator.SetBool(IS_DEATH, true);
+        _spriteRenderer.sortingOrder = -1;
+        _shadow.SetActive(false);
     }
 }
