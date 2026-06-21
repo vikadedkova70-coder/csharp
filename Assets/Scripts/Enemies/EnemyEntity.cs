f5b60b53762aae3be87ca631d15f34559bbe904c
Vika <vikadedkova70@gmail.com>
2026-06-21 17:18:22 +0500

    Eighteenth Commit
    Атака скелета

--- Assets/Scripts/Enemies/EnemyEntity.cs
+++ Assets/Scripts/Enemies/EnemyEntity.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 
+[RequireComponent (typeof(PolygonCollider2D))]
 public class EnemyEntity : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
 
     private int _currentHealth;
 
+    private PolygonCollider2D _polygonCollider2D;
+
+    private void Awake()
+    {
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
+
     private void Start()
     {
         _currentHealth = _maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Attack");
     }
 
     public void TakeDamage(int damage)
@@ -18,7 +31,17 @@ public class EnemyEntity : MonoBehaviour
         DetectDeath();
     }
 
-    public void DetectDeath()
+    public void PolygonColliderTurnOff()
+    {
+        _polygonCollider2D.enabled = false;
+    }
+
+    public void PolygonColliderTurnOn()
+    {
+        _polygonCollider2D.enabled = true;
+    }
+
+    private void DetectDeath()
     {
         if (_currentHealth <= 0)
         {
--- Assets/Scripts/Enemies/SkeletonVisual.cs
+++ Assets/Scripts/Enemies/SkeletonVisual.cs
@@ -1,21 +1,50 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class SkeletonVisual : MonoBehaviour
 {
     [SerializeField] private EnemyAI _enemyAI;
+    [SerializeField] private EnemyEntity _enemyEntity;
+
     private Animator _animator;
 
     private const string IS_RUNNING = "IsRunning";
     private const string CHASING_SPEED_MULTIPLIER = "ChasingSpeedMultiplier";
+    private const string ATTACK = "Attack";
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        _enemyAI.OnEnemyAttack += _enemyAI_OnEnemyAttack;
+    }
+
+    private void OnDestroy()
+    {
+        _enemyAI.OnEnemyAttack -= _enemyAI_OnEnemyAttack;
+    }
+
     private void Update()
     {
         _animator.SetBool(IS_RUNNING, _enemyAI.IsRunning());
         _animator.SetFloat(CHASING_SPEED_MULTIPLIER, _enemyAI.GetRoaningAnimationSpeed());
+    }
+
+    public void TriggerAttackAnimationTurnOff()
+    {
+        _enemyEntity.PolygonColliderTurnOff();
+    }
+
+    public void TriggerAttackAnimationTurnOn()
+    {
+        _enemyEntity.PolygonColliderTurnOn();
+    }
+
+    private void _enemyAI_OnEnemyAttack(object sender, System.EventArgs e)
+    {
+        _animator.SetTrigger(ATTACK);
     }
 }
